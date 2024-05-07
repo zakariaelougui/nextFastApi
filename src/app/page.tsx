6127b5b914485182b8e8ebcce139645b3a9d8c62
Zak <zakariaelougui@gmail.com>
2024-05-07 14:58:34 +0200

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,31 @@
+"use client";
+import { useEffect, useState } from "react";
+
+export default function Home() {
+  const [input, setInput] = useState<string>("");
+
+  const [searchResults, setSearchResults] = useState<{
+    results: string[];
+    duration: number;
+  }>();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      if (!input) return setSearchResults(undefined);
+
+      const res = await fetch(`/api/search?q=${input}`);
+    };
+
+    fetchData();
+  }, [input]);
+
+  return (
+    <div>
+      <input
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        type="text"
+      />
+    </div>
+  );
+}
